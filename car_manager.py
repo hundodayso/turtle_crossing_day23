db195084c8abbc7737ab55221e9faf1f1018b527
Admin <solder-elision.0y@icloud.com>
2024-02-08 22:16:25 +0000

    Starting!

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,11 @@
 from turtle import Turtle
+import random
 
 
 class CarManager(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.shape("square")
+        self.penup()
+        self.setpos(300, random.randint(-250,250))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from turtle import Screen, Turtle
 from player import Player
-# from car_manager import CarManager
+from car_manager import CarManager
 # from scoreboard import Scoreboard
 
 
@@ -10,7 +10,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
-
+car = CarManager()
 
 
 
