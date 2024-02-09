eadb49bf6a6aa9c7292edbeecc135daa4a0f89b1
hundodayso <solder-elision.0y@icloud.com>
2024-02-09 22:01:43 +0000

    cars started!

--- car_manager.py
+++ car_manager.py
@@ -6,10 +6,10 @@ CAR_COLOURS = ["red", "blue", "yellow", "green", "orange", "purple", "magenta"]
 CAR_MOVE_DISTANCE = 5
 
 
-class CarManager():
+class CarManager:
 
     def __init__(self):
-        self.cars = []
+        self.all_cars = []
 
     def new_car(self):
         new_car = Turtle(shape="square")
@@ -17,9 +17,9 @@ class CarManager():
         new_car.color(random.choice(CAR_COLOURS))
         new_car.setpos(START_X_POS, random.randint(-250, 250))
         new_car.shapesize(stretch_wid=1, stretch_len=2)
-        self.cars.append(new_car)
+        self.all_cars.append(new_car)
 
-    def move_car(self):
-        for car in self.cars:
+    def move_cars(self):
+        for car in self.all_cars:
             car_new_xpos = car.xcor() - CAR_MOVE_DISTANCE
             car.setx(car_new_xpos)
--- main.py
+++ main.py
@@ -10,22 +10,26 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
-car = CarManager()
+car_manager = CarManager()
 count = 0
 
 screen.listen()
 screen.onkeypress(fun=player.move_up, key="w")
 
 game_is_on = True
+car_manager.new_car()
 while game_is_on:
     count += 1
     time.sleep(0.1)
     screen.update()
-    car.move_car()
+    car_manager.move_cars()
     # Add a new car every sixth loop
     if count == 6:
         count = 0
-        car.new_car()
+        car_manager.new_car()
+    for car in car_manager.all_cars:
+        car.distance(player) < 20
+        print("hello")
 
 screen.exitonclick()
 
